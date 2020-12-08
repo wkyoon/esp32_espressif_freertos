7a62e1ca2cab56ef14fa6146e856c6ad421e7410
peter yoon <11924654+wkyoon@users.noreply.github.com>
2020-12-08 15:59:14 +0900

    init

---
+++ main/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "nvs_flash.h"
+
+void hello_task(void *pvParameter)
+{
+    printf("Hello world!\n");
+    for (int i = 10; i >= 0; i--) {
+        printf("Restarting in %d seconds...\n", i);
+        vTaskDelay(1000 / portTICK_RATE_MS);
+    }
+    printf("Restarting now.\n");
+    fflush(stdout);
+    esp_restart();
+}
+ 
+void app_main()
+{
+    nvs_flash_init();
+    xTaskCreate(&hello_task, "hello_task", 2048, NULL, 5, NULL);
+}
